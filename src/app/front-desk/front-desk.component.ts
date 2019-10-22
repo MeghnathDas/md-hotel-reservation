d6a21f5d22d8ad94fa0e52f822f20226066e4a0d
MeghnathDas <meghnath.babin@gmail.com>
2019-10-22 16:52:32 +0530

    Router enabled Beta 4

--- src/app/front-desk/front-desk.component.ts
+++ src/app/front-desk/front-desk.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'front-desk',
-  templateUrl: './front-desk.component.html',
+  template: '<p>This is front desk</p>',
+  // templateUrl: './front-desk.component.html',
   styleUrls: ['./front-desk.component.css']
 })
 export class FrontDeskComponent implements OnInit {
