bf1644e3806b4960ffe1e3b668ff04c05cd10a54
MeghnathDas <meghnath.babin@gmail.com>
2019-10-31 14:43:08 +0530

    Moment JS added and applied

--- src/app/front-desk/check-in/check-in.component.ts
+++ src/app/front-desk/check-in/check-in.component.ts
@@ -4,6 +4,8 @@ import { Observable } from "rxjs";
 import { Room } from "../../models";
 import { FormGroup, FormControl } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date';
+import moment from 'moment';
 
 @Component({
   selector: "app-check-in",
@@ -13,13 +15,16 @@ import { Validators } from '@angular/forms';
 export class CheckInComponent implements OnInit {
   avRooms: Room[] = [];
   totalAmt: number;
+  expChkOutDtInitial = {};
+  minDt = new Date();
+  funcIsDateDisabled;
 
   checkInForm = new FormGroup({
     guestName: new FormControl('', [Validators.required, Validators.minLength(3)]),
     address: new FormControl('', [Validators.required, Validators.minLength(10)]),
     contact: new FormControl('', [Validators.required, Validators.minLength(10)]),
     pax: new FormControl(1, [Validators.required, Validators.min(1)]),
-    chkOutDate: new FormControl({}, [Validators.required]),
+    chkOutDate: new FormControl(new Date(), [Validators.required]),
     selRooms: new FormControl([], [Validators.required, Validators.minLength(1)])
   });
 
@@ -30,19 +35,33 @@ export class CheckInComponent implements OnInit {
     this.roomServ.getRooms().subscribe(dta => {
       this.avRooms = dta;
     });
+
+    const tempDt =  moment().add(1, 'days').toObject();
+    const tempNgbDate = {day: tempDt.date, month: tempDt.months+1, year: tempDt.years}; 
+    this.funcIsDateDisabled = (date: NgbDate, current: {month: number}) => date.before(tempNgbDate);
+    this.checkInForm.controls.chkOutDate.setValue(tempNgbDate);
+    this.checkInForm.updateValueAndValidity();
+
+    this.checkInForm.controls.chkOutDate.valueChanges.subscribe(nwValue => {
+        const dtTo = moment([nwValue.year, nwValue.month-1, nwValue.day]);
+        this.onRoomSelection(this.checkInForm.value.selRooms, nwValue);
+    });
+    this.checkInForm.controls.selRooms.valueChanges.subscribe(nwValue => {
+        this.onRoomSelection(nwValue, this.checkInForm.value.chkOutDate);
+    });
   }
 
-  onRoomSelection(selRooms: Room[]): void {
-    //const chkOutDt = this.checkInForm.value.chkOutDate;
-    //const dtSelected = new Date(chkOutDt.year, chkOutDt.month, chkOutDt.day);
-    //const todayDt = new Date();
-    //const nowDate = new Date(todayDt.getFullYear(), todayDt.getMonth(), todayDt.getDay());
-    
-    //// Explicitly convert Date to Number
-    //const pastDaysOfYear = ( chkOutDt.valueOf() - (nowDate).valueOf() );
-    //console.log(pastDaysOfYear);
+  onRoomSelection(selRooms: Room[], chkOutDt): void {
+    const dtTo = moment([chkOutDt.year, chkOutDt.month-1, chkOutDt.day]);
+    const dtFrom = moment(moment().format('YYYY-MM-DD'), 'YYYY-MM-DD');
 
-   this.totalAmt = selRooms.reduce((sum, current) => sum + current.rate, 0);
+    if (dtTo.isValid()) {
+      this.totalAmt = dtTo.diff(dtFrom, 'days')
+                        * (selRooms.reduce((sum, current) => sum + current.rate, 0));
+    } else {
+      this.totalAmt = undefined;
+    }
+
   }
   doCheckin(): void {
     console.log(this.checkInForm.value);
